c386a0fcd7f71b3a56a0aef10002d96d1625f9d4
Tommy Mark <tmm140@gmail.com>
2013-09-24 22:37:05 -0400

    M4: Made deliverable a Java project for Eclipse modification. Modified Person1 method.

--- M4/src/edu/gatech/oad/antlab/person/Person1.java
+++ M4/src/edu/gatech/oad/antlab/person/Person1.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
- *  @version 1.1
+ *  @author Thomas Mark
+ *  @version 1.2
  */
 public class Person1 {
   /** Holds the persons real name */
@@ -30,8 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  return input.substring(2) + input.substring(0,2);
 	}
 	
 	/**
