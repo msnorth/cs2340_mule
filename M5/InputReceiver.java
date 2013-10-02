5c711d232617d29c2f7759e69013ca414b2609b6
Stephen Conway <sconway30@gatech.edu>
2013-10-02 12:31:57 -0400

    Generalized KeyboardReceiver to InputReceiver for future expansion.

--- M5/InputReceiver.java
+++ M5/InputReceiver.java
@@ -1,39 +1,41 @@
 /**
  * 
  * @author Stephen Conway
- * #M5
- * Last modified:	M5		9/30/13		Stephen Conway
+ * 
+ * Created for:		M5		9/30/13
+ * Last modified:	M5		10/2/13		Stephen Conway
  * 
  * 
  * 
  * Purpose: Interface implemented by anything that would use
- * 			keyboard input. Notably planned for GUIs, Auction,
+ * 			keyboard/AI/network input. Notably planned for GUIs, Auction,
  * 			LandGranter, Sprite, etc.
  */
-public interface KeyboardReceiver {
-	private boolean hasKeyboardFocus;
+public interface InputReceiver {
+	private boolean hasFocus;
 	
 	/**
 	 * @author Stephen Conway #M5
-	 * Called by KeyboardAdapter when focus comes onto object
+	 * Called by KeyboardAdapter or other input channels when focus comes onto object
 	 */
 	public void gainFocus() {
-		hasKeyboardFocus = true;
+		hasFocus = true;
 	}
 	
 	/**
 	 * @author Stephen Conway #M5
-	 * Called by KeyboardAdapter when focus moves away from object
+	 * Called by KeyboardAdapter or other input channels when focus moves away from object
 	 */
 	public void loseFocus() {
-		hasKeyboardFocus = false;
+		hasFocus = false;
 	}
 	
 	/**
 	 * @author #M5
-	 * Method to interpret input from keyboard.
+	 * Method to interpret input from keyboard, AI, or network.
 	 * @param input String representation of input. Either single 
 	 * 					char or a word for special keys (enter)
 	 */
 	public abstract void receiveInput(String input);
 }
+
