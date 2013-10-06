a7bbc08a519e00daa18892fe8b2b4ee378fbb493
Tommy <Tommy@tommy-laptop>
2013-10-06 16:06:13 -0400

    MULE-3: Added initial driver functionality

--- M5/Driver.java
+++ M5/Driver.java
@@ -1,9 +1,16 @@
+import javax.swing.JPanel;
+
+import edu.gatech.mule.ui.MainGameWindow;
+import edu.gatech.mule.utils.*;
+
 /**
  * 
  * @author Stephen Conway
  * 
- * Created for:		M5		9/30/13
- * Last modified:	M5		9/30/13
+ * 		Created for:		M5		9/30/13
+ * 		Modifications:		M5		10/6/13	Thomas Mark
+ * 									Initial Driver fleshing from stub 							
+ * 
  * 
  * 
  * 
@@ -12,13 +19,37 @@
  * 			like KeyboardAdapter.
  * Abstracted because of single instance
  */
-public abstract class Driver {
+public abstract class Driver implements GUIManager{
+	private static JPanel currentPanel;
 	/**
-	 * @author #M5
+	 * @author Thomas Mark
 	 * Application entry point. From here, KeyboardAdapter should be initialized and
-	 * the entry menu should be brough up.
+	 * the entry menu should be brought up.
 	 */
 	public static void main(String[] args) {
+		KeyboardAdapter input = new KeyboardAdapter();
+		MainGameWindow frame = new MainGameWindow(input);
 		
+		PlayerConfigMenu playerConfig = new PlayerConfigMenu();
+		currentPanel = playerConfig;
+		frame.setPanel(playerConfig);
+	}
+	
+	/**
+	 * Driver configures new panels based on input from users.
+	 * 
+	 * @param panel
+	 * @param message
+	 */
+	@Override
+	public void notify(JPanel panel, String message) {
+		if (panel == currentPanel && message.equals("next")) {
+			GameConfigMenu gameConfig = new GameConfigMenu();
+			currentPanel = gameConfig;
+			frame.setPanel(gameConfig);
+			
+		} else if (panel == currentPanel && message.equals("next") {
+			System.out.println("Next milestone!");
+		}
 	}
 }
