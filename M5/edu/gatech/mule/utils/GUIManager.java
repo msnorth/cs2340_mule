d7c5a15cef0a08e561b7dc203689032e8c0dd235
Stephen Conway <sconway30@gatech.edu>
2013-10-06 14:57:37 -0400

    Added interface for GUI feedback

---
+++ M5/edu/gatech/mule/utils/GUIManager.java
@@ -0,0 +1,19 @@
+package edu.gatech.mule.utils;
+
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author Stephen Conway
+ * 
+ *         Created for: 	M5 10/6/13 
+ *         Modifiecations:	M5 10/6/13		Stephen Conway
+ *         					Initial creation
+ * 
+ * 
+ * 
+ *         Purpose: implemented by anything expecting feedback from GUI
+ */
+public interface GUIManager {
+	public void notify(JPanel panel, String message);
+}
