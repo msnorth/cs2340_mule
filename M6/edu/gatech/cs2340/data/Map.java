fa33c5abb8420a19c3ff57d99bfe5a15f5dde4ae
Maddy North <madeleine.north@spearstone.com>
2013-10-09 15:20:21 -0400

    Adding methods for tile id, implementing map generation and map functionality

--- M6/edu/gatech/cs2340/data/Map.java
+++ M6/edu/gatech/cs2340/data/Map.java
@@ -1,72 +1,169 @@
 package edu.gatech.cs2340.data;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.logging.Logger;
 
 /**
  * 
- * @author Stephen Conway
- * 		Function group:		Model: Data manager
- * 		Created for:		M6		10/7/13
- * 		Assigned to:		Maddy
- * 		Modifications:								
+ * @author Stephen Conway Function group: Model: Data manager Created for: M6
+ *         10/7/13 Assigned to: Maddy Modifications:
  * 
  * 
  * 
- * 		Purpose: Hold Tiles. Return tiles as needed.
- * 				 
+ *         Purpose: Hold Tiles. Return tiles as needed.
+ * 
  */
-public class Map implements MapResponsibilities{
 
-	
+// 5 x 9 grid
+public class Map implements MapResponsibilities {
+
+	// Can actually handle a map of any size
 	/**
-	 * #M6
-	 * Main constructor. Takes in 44 Tiles to hold.
+	 * #M6 Main constructor. Takes in 44 Tiles to hold.
 	 * 
 	 * @param tiles
 	 */
-	public Map(Tile[] tiles) {
-		
+	// the current title coordinates
+	private int currX;
+	private int currY;
+	private Logger logger;
+
+	private Tile[][] tiles;
+
+	public Map(Tile[][] tiles) {
+		this.tiles = tiles;
+		this.currX = 0;
+		this.currY = 0;
+
+		// for error logging
+		logger = Logger.getGlobal();
 	}
-	
+
+	// increment the current tile coordinates and return the next tile
+	// TODO deal with last tile
 	@Override
 	public Tile getNextTile() {
-		// TODO Auto-generated method stub
-		return null;
+		currX++;
+		currY++;
+		return getTileAt(currX, currY);
+
 	}
 
+	// gets the tile at the given indices. logs error, catches exception, and
+	// returns null if tile
+	// does not exist.
 	@Override
 	public Tile getTileAt(int r, int c) {
-		// TODO Auto-generated method stub
-		return null;
+		Tile t = null;
+		try {
+			t = tiles[r][c];
+
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			logger.log(
+					null,
+					"Goofy goober, don't try and access a tile that doesn't exist",
+					ex);
+			return null;
+
+		}
+		return t;
+
 	}
 
+	// NOTE: Has to work with MapSprite which has different global coordinates
+	// than the tiles
+	// onverts global coordinates to approximate map coordinates.
+	// will return null if tile does not exist
 	@Override
 	public Tile getTileAt(double x, double y) {
-		// TODO Auto-generated method stub
-		return null;
+		return getTileAt((int) (x / tiles.length), (int) (y / tiles[0].length));
 	}
 
+	// get next tile by row
+	// mod ndx by # of tiles in a row
+	// will return null if tile does not exist
 	@Override
 	public Tile getTileNumber(int ndx) {
-		// TODO Auto-generated method stub
-		return null;
+		// to get col, mod ndx by # of tiles in a row (# of columns)
+		int col = ndx / tiles.length;
+		int row = (ndx - col) / tiles[0].length;
+
+		return getTileAt(row, col);
 	}
 
+	// gets the next unknown tile, setting pointers to point to it
+	// will return null if all tiles are owned
 	@Override
 	public Tile getNextUnownedTile() {
-		// TODO Auto-generated method stub
+		Tile t = null;
+		do {
+			t = getTileAt(currX, currY);
+			if (t != null && t.getOwner() == null) {
+				return t;
+			}
+			currX++;
+			currY++;
+		} while (t != null);
+		logger.log(null, "All tiles are owned");
 		return null;
 	}
 
+	// creates a list of unowned tiles and returns a random element from this
+	// list
+	// sets pointers to point to the random unowned tile
 	@Override
 	public Tile getRandomUnownedTile() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Tile> unownedTiles = new HashSet<Tile>();
+		int oldCurrX = currX;
+		int oldCurrY = currY;
+		Tile t = null;
+		do {
+			t = getTileAt(currX, currY);
+			if (t != null && t.getOwner() == null) {
+				unownedTiles.add(t);
+			}
+			currX++;
+			currY++;
+		} while (t != null);
+
+		if (!unownedTiles.isEmpty()) {
+			Random gen = new Random();
+			Tile[] unowned = (Tile[]) unownedTiles.toArray();
+			t = unowned[gen.nextInt(unownedTiles.size())];
+			setCurrentTile(t);
+			return t;
+		} else {
+			// log and reset pointer
+			logger.log(null, "All tiles are owned");
+			currX = oldCurrX;
+			currY = oldCurrY;
+			return null;
+		}
 	}
 
+	// helper method to get x y coordinates of tile
+	// and set currX and currY
+	private void setCurrentTile(Tile t) {
+		outerloop: for (int i = 0; i < tiles.length; i++) {
+			for (int j = 0; j < tiles[0].length; j++) {
+				if (tiles[i][j].compareTo(t)) {
+					currX = i;
+					currY = j;
+					break outerloop;
+				}
+			}
+		}
+
+	}
+
+	// resets ownership of next tile
+	// sets current tile pointers to next tile
 	@Override
 	public void resetNextUnownedTile() {
-		// TODO Auto-generated method stub
-		
+		Tile t = getNextUnownedTile();
+		t.setOwner(null);
 	}
 
 }
--- M6/edu/gatech/cs2340/data/Tile.java
+++ M6/edu/gatech/cs2340/data/Tile.java
@@ -1,44 +1,70 @@
 package edu.gatech.cs2340.data;
-import edu.gatech.cs2340.ui.TileRenderer;
 
+import edu.gatech.cs2340.ui.TileRenderer;
 
 /**
  * 
- * @author Stephen Conway
- * 		Function group:		Model: Data holder
- * 		Created for:		M6		10/7/13
- * 		Assigned to:		Maddy
- * 		Modifications:								
+ * @author Stephen Conway Function group: Model: Data holder Created for: M6
+ *         10/7/13 Assigned to: Maddy Modifications:
  * 
  * 
  * 
- * 		Purpose: Parent class for all Tile objects that make up a grid on the game's map. 				 
+ *         Purpose: Parent class for all Tile objects that make up a grid on the
+ *         game's map.
  */
-public abstract class Tile {
-	//Tile and TileRenderer are paired as Model and View of the Tile concept
+public  class Tile {
+	// Tile and TileRenderer are paired as Model and View of the Tile concept
 	private TileRenderer renderer;
 	private Player owner;
-	
-	
+	private String id;
+
+	public Tile(String id, Player owner, TileRenderer renderer) {
+		this.renderer = renderer;
+		this.owner = owner;
+		this.id = id;
+	}
+
 	/**
-	 * #FUTURE
-	 * Method to get the production of a Tile. Based upon Tile type, MULE present, and 
-	 * 	random events.
+	 * #FUTURE Method to get the production of a Tile. Based upon Tile type,
+	 * MULE present, and random events.
 	 * 
 	 * @return
 	 */
-	 public abstract ResourceAmount calculateProduction();
-	 
-	
+	public ResourceAmount calculateProduction() {
+		return null;
+	}
+
 	/**
-	 * #M6
-	 * Method to set owner of tile.
-	 * Should handle "null" as returning Tile to neutral (through sale)
-	 * Should call something to update its TileRenderer to reflect the change
+	 * #M6 Method to set owner of tile. Should handle "null" as returning Tile
+	 * to neutral (through sale) Should call something to update its
+	 * TileRenderer to reflect the change
 	 * 
 	 * @param newOwner
 	 */
 	public void setOwner(Player newOwner) {
-		
+		this.owner = newOwner;
+		// TODO update tileRenderer
+	}
+
+	/**
+	 * #M6 Method to get owner of tile. Should return null if tile is unowned
+	 * 
+	 * @return
+	 */
+	public Player getOwner() {
+		return this.owner;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	// returns true if equal, false otherwise
+	public boolean compareTo(Tile t) {
+		return (id.equals(t.getId()));
 	}
 }
