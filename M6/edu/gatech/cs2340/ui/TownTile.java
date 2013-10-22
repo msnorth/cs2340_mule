f4accc8c5015d5003f1a429e8cd2ba9a0fde0a41
Stephen Conway <sconway30@gatech.edu>
2013-10-22 16:06:33 -0400

    Readded dummy Player to own TownTile so it is not purchasable.

--- M6/edu/gatech/cs2340/ui/TownTile.java
+++ M6/edu/gatech/cs2340/ui/TownTile.java
@@ -1,5 +1,7 @@
 package edu.gatech.cs2340.ui;
 
+import java.awt.Color;
+
 import edu.gatech.cs2340.data.Player;
 import edu.gatech.cs2340.data.ResourceAmount;
 import edu.gatech.cs2340.data.Tile;
@@ -8,7 +10,8 @@ public class TownTile extends Tile {
 	public static String name = "Town";
 	public TownTile(String id, Player owner) {
 		super(name);
-		this.setOwner(null);
+		Player player = new Player("Municipatily","Human",new Color(255,255,255));
+		this.setOwner(player);
 		this.setId(id);
 		image = townImage; // get static image from superclass
 	}
