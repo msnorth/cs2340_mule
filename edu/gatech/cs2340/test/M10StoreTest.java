45e48d4ed64502cc6f2d620c7044f0242312b3c7
Tommy <Tommy@tommy-laptop>
2013-11-19 17:43:17 -0500

    MULE-69: Javadoc'd and modified test.

--- edu/gatech/cs2340/test/M10StoreTest.java
+++ edu/gatech/cs2340/test/M10StoreTest.java
@@ -16,6 +16,7 @@ import edu.gatech.cs2340.data.Store;
 /**
  * @author Thomas Mark
  *
+ *	Purpose: To test the functionality of the store selling resources to the player.
  */
 public class M10StoreTest {
 	// Player starts with 1000 money
@@ -31,20 +32,30 @@ public class M10StoreTest {
 	public void setUp() throws Exception {
 		store.setPlayer(player);
 	}
-
+	
+	/**
+	 * Test: Correctly checks for the player having enough money.
+	 */
 	@Test
 	public void notEnoughMoney() {
-		assertFalse("The player could buy all 150 units of food.", 
+		assertFalse("The player could buy 150 units of food.", 
 				   store.sellResources(ResourceType.FOOD, 150));
 	}
 	
+	/**
+	 * Test: Correctly checks for the store having enough resources.
+	 */
 	@Test
 	public void notEnoughResources() {
 		player.addMoney(1000000);
-		assertFalse("The store could sell 200 units of Food",
-				    store.sellResources(ResourceType.FOOD, 200));
+		assertFalse("The store could sell 17 units of Food",
+				    store.sellResources(ResourceType.FOOD, 17));
 	}
 	
+	/**
+	 * Test: The method allows the player to purchase a resource if they have enough money and the
+	 * 		 store has enough resources of that particular type.
+	 */
 	@Test
 	public void enoughMoneyAndResources() {
 		assertTrue("The could not buy 10 units of food with 1000 money", 
